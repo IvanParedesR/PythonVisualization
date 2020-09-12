c095049e25ef0b62d4452f3090aee620702c4b54
Ivan Paredes <ivanresendiz25@gmail.com>
2020-09-11 23:42:20 -0500

    Local change

---
+++ Config.R
@@ -0,0 +1,14 @@
+
+py_config()
+
+use_python("/iparedes/AppData/Local/r-miniconda/envs/r-reticulate/python")
+use_virtualenv("~/myenv")
+use_condaenv("myenv")
+
+py_install("pandas")
+
++++++++++++++++++
+  
+  
+  
+  
---
+++ Pruebas.py
@@ -0,0 +1,14 @@
+# Introduction to Visualization with Python – Basic and Customized Plotting
+
+# AOpen a jupyter notebook and load the pandas and seaborn libraries:
+#Load pandas library
+
+import pandas as pd
+import seaborn as sns
+
+#URL of the dataset
+diamonds_url = "https://raw.githubusercontent.com/TrainingByPackt/Interactive-Data-Visualization-with-Python/master/datasets/diamonds.csv"
+#Yes, we can read files from a URL straight into a pandas DataFrame!
+diamonds_df = pd.read_csv(diamonds_url)
+# Since the dataset is available in seaborn, we can alternatively read it in using the following line of code
+diamonds_df = sns.load_dataset('diamonds')
